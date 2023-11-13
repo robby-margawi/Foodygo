4d643431a5090d074725a747eec6a0891f3de432
Robby Margawi <robby_margawi_gibeon@hotmail.com>
2023-11-13 17:43:05 +0700

    change css path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './dist/css/main.css'
+import './css/main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { MetaMaskUIProvider } from '@metamask/sdk-react-ui';
 
